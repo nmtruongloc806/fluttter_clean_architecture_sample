0f265550a627f541220c17b43a6edb5d939d7157
locnmt <locnmt@vietinfo.tech>
2024-07-22 17:42:13 +0700

    Update document_repository.dart

--- fluttter_clean_architecture_sample/lib/domain/repositories/document_repository.dart
+++ fluttter_clean_architecture_sample/lib/domain/repositories/document_repository.dart
@@ -1,3 +1,5 @@
+import 'package:fluttter_clean_architecture_sample/domain/entities/document.dart';
+
 abstract class DocumentRepository {
   Future<bool> saveDocument(Document document);
   Future<List<Document>> getDocuments();
